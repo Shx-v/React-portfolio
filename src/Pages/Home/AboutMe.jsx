d22778b5632695a44a01d07b120775b0a4d32cbe
shiv <shivranjanbharadwaj@gmail.com>
2023-09-13 12:55:20 +0530

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,30 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hello, I'm Shivranjan Bharadwaj, an Engineer deeply passionate about
+            Fullstack Development. I thrive on the challenge of Development, and
+            I bring a creative flair to every project I undertake. With a solid
+            foundation in different frameworks.
+          </p>
+          <p className="hero--section-description">
+            Beyond my professional life, I find inspiration in gaming. These
+            experiences not only recharge my creativity but also bring a fresh
+            perspective to my work. I'm excited to become a successful
+            developer. If you're interested in working together or connecting,
+            please don't hesitate to reach out through Email. Thank you for
+            visiting my portfolio, and I'm eager to explore new opportunities
+            and connections with you.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-scroll";
+
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Shivranjan</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <button className="btn btn-primary"><Link to='Contact'>Get In Touch</Link></button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_image.svg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
